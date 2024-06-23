05e82ce9d9880bb81f922116a7cbc56e31dd96b6
Iustin2131 <126079873+Iustin2131@users.noreply.github.com>
2024-06-23 08:41:14 +0300

    conectare baza de date

--- database.php
+++ database.php
@@ -1,16 +1,25 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $password = "";
-    $database = "fosa";
-    $connection = "";
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "fosa";
+$connection = "";
 
-    try {
-        $connection = mysqli_connect($host, $user, $password, $database);
-    }
-    catch(mysqli_sql_exception $e)
-    {
-        echo "Connection failed: " . $e->getMessage();
-    }
+// Verifică dacă ești pe Heroku
+if (getenv("JAWSDB_URL")) {
+    $url = parse_url(getenv("JAWSDB_URL"));
+    $host = $url["host"];
+    $user = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+}
 
+try {
+    $connection = mysqli_connect($host, $user, $password, $database);
+    if (!$connection) {
+        throw new Exception("Connection failed: " . mysqli_connect_error());
+    }
+} catch (Exception $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'iustin/fosa',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6ba62672d25fa0ecc26de87ba3c6570d2a6f25e4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'iustin/fosa' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6ba62672d25fa0ecc26de87ba3c6570d2a6f25e4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
